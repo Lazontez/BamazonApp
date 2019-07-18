5938977ecf0e65314ca22416196d4ae80487c611
Tez, Gardner <tezgardner1212@gmail.com>
2019-07-18 11:16:55 -0500

    cleaned up code

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale.
-//  Include the ids, names, and prices of products for sale.
+
 
 var mysql = require("mysql");
 var inquirer = require("inquirer")
